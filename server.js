6a3f67d006e8f4c4ae306b0b21ac61ef85d1415b
murray918 <darbyguitars@gmail.com>
2019-03-06 23:23:28 -0600

    we have a message BABY!!!

--- server.js
+++ server.js
@@ -7,14 +7,21 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html")
 })
 
-//initialize connection
+//initialize connection this needs to be done for each of the things you are doing i will go into more detail about this later
 io.on("connection", (socket) => {
   console.log("hello i am the connection and i can sing!!!!")
-  socket.on('disconnect', function(){
-    console.log('user cannot sing anymore');
-  });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg)
+  })
+
+  io.on("connection", function(socket) {
+    console.log("a user connected")
+    socket.on("disconnect", function() {
+      console.log("user disconnected")
+    })
+  })
 })
- 
+
 //initiate the server
 server.listen(3000, () => {
   console.log("server is listening on port 3000")

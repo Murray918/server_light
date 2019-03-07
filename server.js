bf757380dc95da493fbb5af98d3f2963c18a8cc3
murray918 <darbyguitars@gmail.com>
2019-03-06 22:01:10 -0600

    getting some kind of socket.io chat rolling for testing how sockets work

---
+++ server.js
@@ -0,0 +1,32 @@
+const app = require("express")()
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+
+
+app.get('/', (request, response) => {
+    console.log(request.header)
+    response.send('<h1>Hello World</h1>')
+})
+
+//initialize connection
+io.on("connection", () => {
+  console.log("hello")
+
+  // emit an event to the socket
+  socket.emit("request", () => {
+    console.log("this is a request", request)
+  })
+
+  // emit an event to all
+  io.emit("broadcast", () => {
+    console.log("broadcasting")
+  })
+
+      // listen to the event
+  socket.on("reply", () => {
+      conole.log('a reply has been sent i guess')
+  }) 
+})
+server.listen(3000, () => {
+  console.log("server is listening on port 3000")
+})
